3c4c11043d51699caf2c84af926d22aa9a9dd36e
Iskander <iskander.andrews@openzeppelin.com>
2025-04-17 10:57:07 +0100

    feat(contracts):  `Queue` Compact data structure (#9)
    * feat: queue compact contract is ready
    
    * chore: adding TODO comment to test tail overflow
    
    * chore: remove .envrc
    
    * feat: queue compact contract is ready
    
    * chore: rebase
    
    * chore: rebase

---
+++ contracts/structs/src/witnesses/QueueWitnesses.ts
@@ -0,0 +1,9 @@
+import type { EmptyState } from '../types/state';
+
+export type QueueContractPrivateState = EmptyState;
+
+export const QueueContractPrivateState = {
+  generate: (): EmptyState => ({}),
+};
+
+export const QueueWitnesses = {};
