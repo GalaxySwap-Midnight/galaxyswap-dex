65c80b4989a31dd0fe0c76a122b8617ad10231d0
Iskander <iskander.andrews@openzeppelin.com>
2025-05-19 10:16:05 +0200

    Update contracts/math/src/witnesses/MathU64.ts
    Co-authored-by: Andrew Fleming <fleming.andrew@protonmail.com>

--- contracts/math/src/witnesses/MathU64.ts
+++ contracts/math/src/witnesses/MathU64.ts
@@ -48,7 +48,7 @@ export const MathU64Witnesses =
     },
 
     /**
-     * @description Computes division of two Uint<128> values off-chain.
+     * @description Computes division of two Uint<64> values off-chain.
      * @param context - The witness context containing ledger and private state.
      * @param dividend - The number to divide.
      * @param divisor - The number to divide by.
