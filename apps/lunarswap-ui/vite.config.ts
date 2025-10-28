dfe4ccba2ab9682b1621c22258690b0ed70f9818
0xisk <iskander.andrews@openzeppelin.com>
2025-10-28 11:49:34 +0100

    fix: build issue

--- apps/lunarswap-ui/vite.config.ts
+++ apps/lunarswap-ui/vite.config.ts
@@ -7,7 +7,6 @@ import wasm from 'vite-plugin-wasm';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lunarswap/',
   cacheDir: './.vite',
   build: {
     target: 'esnext',
